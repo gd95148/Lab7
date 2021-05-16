db55fc876049f8ac5f9277c5d9036b5cf0aaaf5f
atramUCSD <atram@ucsd.edu>
2021-05-15 19:23:57 -0700

    Filled out sw.js and added part2b.png

--- sw.js
+++ sw.js
@@ -1,6 +1,88 @@
 // sw.js - Service Worker
 
 // You will need 3 event listeners:
-//   - One for installation
-//   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('./sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  'https://cse110lab6.herokuapp.com/entries',];
+
+//   - One for installation
+self.addEventListener('install', function(event) {
+
+    self.skipWaiting();
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+//   - One for fetch requests
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+      })
+  );
+});
+
+//   - One for activation ( check out MDN's clients.claim() for this step )
+self.addEventListener('activate', function(event) {
+
+    var cacheAllowlist = [CACHE_NAME];
+
+    self.skipWaiting();
+  
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheAllowlist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
